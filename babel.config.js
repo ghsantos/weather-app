c541548acfa4c983895ffe7ad794368019a48522
ghsantos <ghsantos933@gmail.com>
2025-05-02 17:28:06 -0300

    initial commit

---
+++ babel.config.js
@@ -0,0 +1,25 @@
+module.exports = function(api) {
+  api.cache(true);
+  let plugins = [];
+
+  
+
+    
+        plugins.push([
+            'react-native-unistyles/plugin',
+            {
+                autoProcessRoot: 'app',
+                autoProcessImports: ['~/components']
+            }
+        ]);
+    
+
+  
+
+  return {
+    
+      presets: ['babel-preset-expo'],
+    
+    plugins,
+  };
+};
